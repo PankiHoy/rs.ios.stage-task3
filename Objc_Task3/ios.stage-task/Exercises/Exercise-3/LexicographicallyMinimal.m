3bbc66fda1866715047e27c290189a758f293714
Maksim Karaseu <karaseu.maksim@gmail.com>
2021-05-21 18:45:43 +0300

    feat added implementation for exercise 2, and some for ex 3

--- Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
@@ -9,7 +9,40 @@
 @implementation LexicographicallyMinimal
 
 -(NSString *)findLexicographicallyMinimalForString1:(NSString *)string1 andString2:(NSString *)string2 {
-    return nil;
+    NSMutableString *resultString = [[NSMutableString alloc] init];
+    
+    unichar buffer1[string1.length+1], buffer2[string2.length+1];
+    [string1 getCharacters:buffer1];
+    [string2 getCharacters:buffer2];
+    
+    NSMutableArray *stringArray1 = [[NSMutableArray alloc] init];
+    NSMutableArray *stringArray2 = [[NSMutableArray alloc] init];
+    
+    for (int i=0; i<string1.length; i++) {
+        [stringArray1 insertObject:[NSString stringWithFormat:@"%c", buffer1[i]] atIndex:0];
+    }
+    for (int i=0; i<string2.length; i++) {
+        [stringArray2 insertObject:[NSString stringWithFormat:@"%c", buffer2[i]] atIndex:0];
+    }
+    
+    while (stringArray1.count != 0 || stringArray2.count != 0) {
+        if ([[stringArray1 lastObject] compare:[stringArray2 lastObject]] == NSOrderedAscending) {
+            [resultString appendString:[stringArray1 lastObject]];
+            [stringArray1 removeLastObject];
+        } else if ([[stringArray1 lastObject] compare:[stringArray2 lastObject]] == NSOrderedDescending){
+            [resultString appendString:[stringArray2 lastObject]];
+            [stringArray2 removeLastObject];
+        }
+    }
+    
+    for (int i=0; i<stringArray1.count; i++) {
+        [resultString insertString:stringArray1[i] atIndex:0];
+    }
+    for (int i=0; i<stringArray2.count; i++) {
+        [resultString insertString:stringArray2[i] atIndex:0];
+    }
+    
+    return resultString;
 }
 
 @end
